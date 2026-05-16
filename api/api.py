4c6d5e60355c98a2bca7bacb06c7f515c132a6e4
Elie Martila -- van Beek <elie.vanbeek66@gmail.com>
2026-05-16 15:37:09 +0200

    feat: new bask services that returns all musics

--- api/api.py
+++ api/api.py
@@ -23,6 +23,18 @@ def get_music(playlist):
     rd.shuffle(music)
     return jsonify({"music": music})
 
+@app.route('/musics', methos=['GET'])
+def get_musics():
+    directory_path = "/app/music" #path in container
+    playlists = [entry for entry in os.listdir(directory_path) 
+    if os.path.isdir(os.path.join(directory_path, entry))]
+    musics = []
+    for playlist in playlists:
+        musics.append([entry for entry in os.listdir(directory_path + playlist) 
+                    if os.path.isfile(os.path.join(directory_path + playlist, entry))])
+    return jsonify({"musics": musics})
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
